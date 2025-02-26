b0e269aa2242ce525db9fb30c9b4b14413093a74
Will <w.j.colgate@gmail.com>
2025-02-26 22:39:22 +0800

    docs: Board docs

--- Board.java
+++ Board.java
@@ -1,24 +1,34 @@
 /**
- * 
+ * <p>
+ * This class keeps track of the state of the board and has methods to
+ * initialise, get and update the board.
+ * </p>
  */
 public class Board {
     /**
-     * 
+     * <p>
+     * Declare the 2D char array that will represent the board.
+     * </p>
      */
     private char[][] board;
 
     /**
-     * 
+     * <p>
+     * Constructor method that initialises the board.
+     * </p>
      */
     public Board() {
         this.board = initBoard(6, 7);
     }
 
     /**
+     * <p>
+     * Method that generates the board with empty spaces.
+     * </p>
      * 
-     * @param cols
-     * @param rows
-     * @return
+     * @param cols Number of cols.
+     * @param rows Number of rows.
+     * @return Return a 2D char array to represent the board.
      */
     private char[][] initBoard(int cols, int rows) {
         char[][] board = new char[cols][rows];
@@ -31,26 +41,35 @@ public class Board {
     }
 
     /**
+     * <p>
+     * Getter method that returns the current board.
+     * </p>
      * 
-     * @return
+     * @return 2D array of the current board.
      */
     public char[][] getBoard() {
         return this.board;
     }
 
     /**
+     * <p>
+     * Setter method that updates the board based on a given colour and coordinate.
+     * </p>
      * 
-     * @param pos
-     * @param colour
+     * @param pos    Coordinate array in format [row, column].
+     * @param colour Char r or y.
      */
     public void updateBoard(Integer[] pos, char colour) {
-            this.board[pos[0]][pos[1]] = colour;
+        this.board[pos[0]][pos[1]] = colour;
     }
 
     /**
+     * <p>
+     * Method to return the colour of a given position.
+     * </p>
      * 
-     * @param position
-     * @return
+     * @param position Coordinate array in format [row, column].
+     * @return Char r or y (or an empty char).
      */
     public char getPieceAtPosition(Integer[] position) {
         return this.board[position[0]][position[1]];
