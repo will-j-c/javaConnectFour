b9aaf600e66c3825e8f58da9be8d421c07fb8946
Will <w.j.colgate@gmail.com>
2025-02-26 22:51:52 +0800

    docs: ComputerPlayer docs

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -2,19 +2,27 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
- * 
+ * A class to represent the computer player. Currently takes a random move.
  */
 public class ComputerPlayer extends Player {
     /**
-     * 
+     * <p>
+     * Declaration of the display variable.
+     * </p>
      */
     Display display;
 
     /**
+     * <p>
+     * Constructor method that initialises the computer player using an instance of
+     * the board and display classes and is assigned a colour.
+     * </p>
      * 
-     * @param display
-     * @param board
-     * @param colour
+     * @param display An instance of the Display class initialised by the
+     *                ConnectFour game.
+     * @param board   An instance of the Board class initialised by the ConnectFour
+     *                game.
+     * @param colour  A char representing yellow or red.
      */
     ComputerPlayer(Display display, Board board, char colour) {
         super(board, colour);
@@ -23,7 +31,10 @@ public class ComputerPlayer extends Player {
     }
 
     /**
-     * 
+     * <p>
+     * Implementation of the abstract takeTurn method inherited from Player. Defines
+     * the logic on taking a turn. Records output in lastTurn.
+     * </p>
      */
     @Override
     public void takeTurn(ArrayList<Integer[]> validMoves) {
