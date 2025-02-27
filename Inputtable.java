d968bb6acaebb61c6658e6a877e2d9deca6e1686
Will <w.j.colgate@gmail.com>
2025-02-27 12:37:01 +0800

    docs: Inputtable docs

--- Inputtable.java
+++ Inputtable.java
@@ -1,32 +1,48 @@
 /**
- * 
+ * <p>
+ * User input within a class is enabled by the class implementing Inputtable.
+ * Classes that do not implement this interface cannot implement user input.
+ * </p>
  */
 public interface Inputtable {
     /**
+     * <p>
+     * Governs input of integers.
+     * </p>
      * 
-     * @return
+     * @return int input
      */
     public int intInput();
 
     /**
+     * <p>
+     * Governs input of Stings.
+     * </p>
      * 
-     * @return
+     * @return String input
      */
     public String stringInput();
 
     /**
+     * <p>
+     * Governs input of characters.
+     * </p>
      * 
-     * @return
+     * @return char input
      */
     public char charInput();
 
     /**
-     * 
+     * <p>
+     * Calls the next line of the stream.
+     * </p>
      */
     public void next();
 
     /**
-     * 
+     * <p>
+     * Closes the stream.
+     * </p>
      */
     public void close();
 }
