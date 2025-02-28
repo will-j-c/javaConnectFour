e5ce54a7c00b6ae525322e50caa5d499783ed405
Will <w.j.colgate@gmail.com>
2025-02-28 15:09:03 +0800

    docs: HumanPlayer docs

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -2,24 +2,34 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 
 /**
- * 
+ * <p>
+ * A class to represent a human player. Inherits from Player and adds additional
+ * functionality around inputs and feedback on validity of moves.
+ * </p>
  */
 public class HumanPlayer extends Player {
     /**
-     * 
+     * <p>
+     * Declaration of a variable to hold the player Input instance.
+     * </p>
      */
     Input playerInput;
     /**
-     * 
+     * <p>
+     * Declaration of a variable to hold the Display instance.
+     * </p>
      */
     Display display;
 
     /**
+     * <p>
+     * Constructor to initialise a new player.
+     * </p>
      * 
-     * @param playerInput
-     * @param display
-     * @param board
-     * @param colour
+     * @param playerInput An instance of the Input class.
+     * @param display     The Display instance.
+     * @param board       The board Instance.
+     * @param colour      The player colour character.
      */
     HumanPlayer(Input playerInput, Display display, Board board, char colour) {
         super(board, colour);
@@ -28,7 +38,12 @@ public class HumanPlayer extends Player {
     }
 
     /**
+     * <p>
+     * A method that takes in a list of valid moves, validates user input of a valid
+     * column and sets this.lastMove.
+     * </p>
      * 
+     * @param validMoves ArrayList of valid coordinates.
      */
     @Override
     public void takeTurn(ArrayList<Integer[]> validMoves) {
@@ -61,9 +76,12 @@ public class HumanPlayer extends Player {
     }
 
     /**
+     * <p>
+     * Method to establish the valid columns from a list of valid positions.
+     * </p>
      * 
-     * @param validMoves
-     * @return
+     * @param validMoves ArrayList of valid coordinates.
+     * @return An ArrayList of Integers representing the valid columns.
      */
     private ArrayList<Integer> validCols(ArrayList<Integer[]> validMoves) {
         ArrayList<Integer> validCols = new ArrayList<Integer>();
